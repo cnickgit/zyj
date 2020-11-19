2620ca63310b95c57d8989f7c3293639c70b0a20
wsg <1209930447@qq.com>
2020-11-19 23:37:50 +0800

    我的页面修改

--- src/page/my.vue
+++ src/page/my.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <div style="display: inline-block;" >
-      aaa
-      <span>Lemon✨</span>
-      <span>SVIP3</span><br>
-<!--      <span>今免费可查：500次降权/剩余179次/260天<</span>-->
-    </div>
-    <div style="display: inline-block;">
-      bb
-      <img src="@/assets/132.jpg">
-    </div>
+    <van-row type="flex" justify="center">
+      <van-col span="6">0</van-col>
+      <van-col span="6">0</van-col>
+      <van-col span="6">480</van-col>
+      <van-col span="6">481</van-col>
+    </van-row>
+    <van-row type="flex" justify="center">
+      <van-col span="6">我的照妖币</van-col>
+      <van-col span="6">我的点数</van-col>
+      <van-col span="6">今天已查</van-col>
+      <van-col span="6">昨天已查</van-col>
+    </van-row>
+    <van-cell title="我的查询历史" is-link url="/vant/mobile.html" />
+    <van-cell title="照妖镜使用规则" is-link url="/vant/mobile.html" />
+    <van-row type="flex">
+      <van-col span="3">分享</van-col>
+      <van-col span="3">退出</van-col>
+      <van-col span="5">修改密码</van-col>
+      <van-col span="5">我要充值</van-col>
+      <van-col span="5">我要购点</van-col>
+    </van-row>
   </div>
 </template>
 
 <script>
+    import { Col, Row, Cell, CellGroup } from 'vant';
     export default {
-        name: "my.vue"
+        name: "my.vue",
+        components:{
+            [Col.name]: Col,
+            [Row.name]: Row,
+            [Cell.name]: Cell,
+            [CellGroup.name]: CellGroup,
+        }
     }
 </script>
 
