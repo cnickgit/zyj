5c353fb6eea66e5fe2a71aac755e8cd4681bb405
wsg <1209930447@qq.com>
2020-11-20 14:21:15 +0800

    页面大致结构

--- src/home/homePage.vue
+++ src/home/homePage.vue
@@ -10,28 +10,53 @@
           <div>
             <van-button round type="info" size="large" @click="search()">查询</van-button>
           </div>
+          <div v-if="false">
             <van-row style="margin-top: 20px;" justify="center">
               <van-col span="12">买家: 8</van-col>
               <van-col span="12">实名认证: 8</van-col>
             </van-row>
-          <van-row style="margin-top: 20px;" justify="center">
-            <van-col span="12">买家信誉: 8</van-col>
-            <van-col span="12">注册日期: 8</van-col>
-          </van-row>
-          <van-row style="margin-top: 20px;" justify="center">
-            <van-col span="12">商家信誉: 8</van-col>
-            <van-col span="12">	淘龄: 8</van-col>
-          </van-row>
-          <van-row style="margin-top: 20px;" justify="center">
-            <van-col span="12">性别: 8</van-col>
-            <van-col span="12">	买家总周平均: 8</van-col>
-          </van-row>
-          <van-row style="margin-top: 20px;" justify="center">
-            <van-col span="12">好评率: 8</van-col>
-          </van-row>
-          <van-row style="margin-top: 20px;" justify="center">
-            <van-col span="12">查询时间: 8</van-col>
-          </van-row>
+            <van-row style="margin-top: 20px;" justify="center">
+              <van-col span="12">买家信誉: 8</van-col>
+              <van-col span="12">注册日期: 8</van-col>
+            </van-row>
+            <van-row style="margin-top: 20px;" justify="center">
+              <van-col span="12">商家信誉: 8</van-col>
+              <van-col span="12">	淘龄: 8</van-col>
+            </van-row>
+            <van-row style="margin-top: 20px;" justify="center">
+              <van-col span="12">性别: 8</van-col>
+              <van-col span="12">	买家总周平均: 8</van-col>
+            </van-row>
+            <van-row style="margin-top: 20px;" justify="center">
+              <van-col span="12">好评率: 8</van-col>
+            </van-row>
+            <van-row style="margin-top: 20px;" justify="center">
+              <van-col span="12">查询时间: 8</van-col>
+            </van-row>
+          </div>
+          <div v-if="true">
+            <van-row class="content">
+              <van-col span="24">兔子：拿完了商家的返款就恶意退款</van-col>
+            </van-row>
+            <van-row class="content">
+              <van-col span="24">蜜獾：用各种方式威胁你给钱</van-col>
+            </van-row>
+            <van-row class="content">
+              <van-col span="24">狐狸：用各种方式骗你钱了</van-col>
+            </van-row>
+            <van-row class="content">
+              <van-col span="24">鳄鱼：用发票，商标，假货等方式坑你钱</van-col>
+            </van-row>
+            <van-row class="content">
+              <van-col span="24">野狗：接完了单给了你差评进行要挟</van-col>
+            </van-row>
+            <van-row class="content">
+              <van-col span="24">老鼠：用了淘客链接来拍你的单</van-col>
+            </van-row>
+            <van-row class="content">
+              <van-col span="24">降权：导致商家单品降权的帐号</van-col>
+            </van-row>
+          </div>
         </van-tab>
         <van-tab title="标签 2">内容 2</van-tab>
         <van-tab title="标签 3">内容 3</van-tab>
@@ -58,7 +83,7 @@
        data(){
           return {
               active: 0,
-              searchName: ''
+              searchName: '',
           }
        },
        methods: {
@@ -101,5 +126,7 @@
 </script>
 
 <style scoped>
-
+.content{
+  margin-top: 20px;
+}
 </style>
--- src/page/my.vue
+++ src/page/my.vue
@@ -1,33 +1,37 @@
 <template>
   <div>
     <van-row type="flex" justify="center">
-      <van-col span="6">0</van-col>
-      <van-col span="6">0</van-col>
-      <van-col span="6">480</van-col>
-      <van-col span="6">481</van-col>
+      <van-col span="8">40</van-col>
+      <van-col span="8">0</van-col>
+      <van-col span="8">未使用</van-col>
     </van-row>
-    <van-row type="flex" justify="center">
-      <van-col span="6">我的照妖币</van-col>
-      <van-col span="6">我的点数</van-col>
-      <van-col span="6">今天已查</van-col>
-      <van-col span="6">昨天已查</van-col>
+    <van-row type="flex" justify="center" style="margin-top: 10px;">
+      <van-col span="8">今日可用次数</van-col>
+      <van-col span="8">今日已用次数</van-col>
+      <van-col span="8">到期时间</van-col>
     </van-row>
-    <van-cell title="我的查询历史" is-link url="/vant/mobile.html" />
-    <van-cell title="照妖镜使用规则" is-link url="/vant/mobile.html" />
-    <van-row type="flex">
-      <van-col span="3"><van-image :src="require('../assets/share.png')" /></van-col>
-      <van-col span="3"><van-image :src="require('../assets/out.png')" /></van-col>
-      <van-col span="5"><van-image :src="require('../assets/editpsd.png')" /></van-col>
-      <van-col span="5"><van-image :src="require('../assets/buyPoint.png')" /></van-col>
-      <van-col span="5"><van-image :src="require('../assets/rech.png')" /></van-col>
+    <h2>查询记录</h2>
+    <van-row type="flex" justify="center" style="margin-top: 10px;">
+      <van-col span="8">查询内容</van-col>
+      <van-col span="8">查询类型</van-col>
+      <van-col span="8">查询时间</van-col>
     </van-row>
-    <van-row type="flex">
-      <van-col span="3">分享</van-col>
-      <van-col span="3">退出</van-col>
-      <van-col span="5">修改密码</van-col>
-      <van-col span="5">我要充值</van-col>
-      <van-col span="5">我要购点</van-col>
-    </van-row>
+<!--    <van-cell title="我的查询历史" is-link url="/vant/mobile.html" />-->
+<!--    <van-cell title="照妖镜使用规则" is-link url="/vant/mobile.html" />-->
+<!--    <van-row type="flex">-->
+<!--      <van-col span="3"><van-image :src="require('../assets/share.png')" /></van-col>-->
+<!--      <van-col span="3"><van-image :src="require('../assets/out.png')" /></van-col>-->
+<!--      <van-col span="5"><van-image :src="require('../assets/editpsd.png')" /></van-col>-->
+<!--      <van-col span="5"><van-image :src="require('../assets/buyPoint.png')" /></van-col>-->
+<!--      <van-col span="5"><van-image :src="require('../assets/rech.png')" /></van-col>-->
+<!--    </van-row>-->
+<!--    <van-row type="flex">-->
+<!--      <van-col span="3">分享</van-col>-->
+<!--      <van-col span="3">退出</van-col>-->
+<!--      <van-col span="5">修改密码</van-col>-->
+<!--      <van-col span="5">我要充值</van-col>-->
+<!--      <van-col span="5">我要购点</van-col>-->
+<!--    </van-row>-->
   </div>
 </template>
 
